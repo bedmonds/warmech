389a9ef458a4ca90e3049002b0e5732b100aad8d
Brian Edmonds <brian@bedmonds.net>
2018-01-05 16:28:17 -0500

    Add league schedule querying via !schedule command
    Adds a !schedule command that queries the FFR League Schedule,
    returning a list of upcoming matches.

--- src/warmech/races.ts
+++ src/warmech/races.ts
@@ -1,6 +1,100 @@
-import { IStream } from './warmech';
+import { execFile } from 'child_process';
+import * as root from 'app-root-path';
 
-// Print a list of active FFR Races to a stream.
-export function list(os: IStream) {
+import Datastore = require('nedb');
 
+import { IRepo } from './warmech';
+
+
+type RaceState = 'open' | 'in_progress' | 'complete' | 'unknown';
+
+class Race {
+	constructor(
+		public game: string, 
+		public room: string, 
+		public state: RaceState, 
+		public goal: string,
+		public discovered_at: Date
+	) {}
 }
+
+
+const $db = new Datastore({
+	filename: `${root}/db/races.db`,
+	autoload: true
+});
+
+
+// Update race history by retrieving current races
+// from the SRL API and removing stale races.
+//
+// This whole chain is criminally inefficient, but
+// we're not dealing with large datasets and it's my
+// fucking hardware running it.
+export function refresh(db: IRepo) {
+	db = db || $db;
+
+	removeOldRaces(db);
+	list(function(err, races) {
+		if (err || races == undefined) throw err;
+
+		for(let i of races) createOrUpdate(db, i);
+	});
+}
+
+
+function createOrUpdate(db: IRepo, race: Race) {
+	db.find({ 'room': race.room }, function(err, doc) {
+		throw new Error('NYI');
+	});
+};
+
+
+// Remove any stored race data older than 24 hours.
+//
+// If a Speed Run takes more than 24 hours, there's something wrong.
+function removeOldRaces(db: IRepo) {
+	let q = { 'discovered_at': {
+		$lt: (new Date(Date.now() - 24 * 3600 * 1000))
+	}};
+
+	db.remove(q, function(err, data) {
+		if (err) throw err;	
+	});
+}
+
+
+// Parse the output of `bin/fetch-races` into an array of `Race`s.
+//
+// See that program for formatting.
+function parse(data: string) {
+	let races: Race[] = data.trim().split("\n").map(function(i) {
+		var args = i.split("\t"); 
+		return new Race(
+			args[0], args[1], <RaceState>args[2], args[3], new Date()
+		);
+	});
+
+	return races;
+}
+
+
+function list(cb: (err: Error | null, races: Race[] | undefined) => void) {
+	let ls = execFile(`${root}/bin/fetch-races`, ['ffhacks']);
+	
+	ls.stdout.on('data', function(data) {
+		cb(null, parse(data.toString()));
+	});
+
+	ls.stderr.on('data', function(data) {
+		cb(new Error(data.toString()), undefined);
+	});
+}
+
+
+
+//
+// Exports for unit tests
+//
+export var _parse = parse;
+export var _list = list;
---
+++ t/races.spec.ts
@@ -0,0 +1,76 @@
+// Test that race-related functionality works properly.
+//
+// Of note is that not only the public API of the race module
+// is tested here.
+//
+// Due to the nature of SRL, some of these tests will fail if you are not
+// connected to the internet, and may sometimes time out.
+import 'mocha';
+import * as assert from 'assert';
+import * as root from 'app-root-path';
+import { execFile } from 'child_process';
+
+import * as races from '../src/warmech/races';
+
+
+const dummyRaces = `Final Fantasy Hacks\t#srl-yojoe\topen\tKill Chaos
+The Legend of Zelda: A link to the Past Hacks\t#srl-alwaysthere\tin_progress\tvtwhatever randomizer
+Mega Man X\t#srl-calebhart\tcomplete\tany%
+
+
+`;
+
+
+describe('race listing', function() {
+	it('should return a list', function(done) {
+		// SRL had a tendancy to take longer than 2 seconds
+		// every once in a while.
+		this.timeout(8000);
+
+		races._list(function(err, races) {
+			assert.ifError(err);
+			assert.ok(races);
+			done();
+		});
+	});
+
+	it('should parse fetch-races output', function(done) {
+		let list = races._parse(dummyRaces);
+
+		assert.equal(3, list.length);
+
+		assert.equal('Final Fantasy Hacks', list[0].game);
+		assert.equal('complete', list[2].state);
+
+		done();
+	});
+});
+
+
+describe('race history', function() {
+	it('should remove stale races', function(done) {
+		// [TODO] Implement me
+		assert.ifError(new Error('NYI'));
+		done();
+	});
+
+	it('should update existing race goals', function(done) {
+		// [TODO] Implement me
+		assert.ifError(new Error('NYI'));
+		done();
+	});
+});
+
+describe('srl-api interop', function() {
+	it('should fetch current races', function(done) {
+		// SRL had a tendancy to take longer than 2 seconds
+		// every once in a while.
+		this.timeout(8000);
+
+		var ls = execFile(`${root}/bin/fetch-races`, []);
+		ls.stdout.on('data', function(data) {
+			assert.ok(data);
+			done();
+		});
+	});
+});
